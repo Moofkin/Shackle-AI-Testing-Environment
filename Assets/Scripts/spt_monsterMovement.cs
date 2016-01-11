520b431d8d11427cede75bea2012b12204aecfe4
Moofkin <cclauren15@gmail.com>
2016-01-10 20:20:12 -0800

    Second Commit
    Introduced obstacles to testing environment; AI did well with object
    avoidance.

--- Assets/Scripts/spt_monsterMovement.cs
+++ Assets/Scripts/spt_monsterMovement.cs
@@ -17,10 +17,13 @@ public class spt_monsterMovement : MonoBehaviour {
 
     // Use this for initialization
 	void Start () {
+
+        //Gets the waypoint graph from another script, then sets the first waypoint to the center of the room.
         graphScript = GameObject.FindObjectOfType(typeof(spt_createGraphForTestingEnvironment)) as spt_createGraphForTestingEnvironment;
         waypointGraph = graphScript.getWaypointGraph();
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(waypoints[22].position);
+        agent.SetDestination(waypoints[26].position);
+        currentWaypoint = 26;
 	}
 	
 	// Update is called once per frame
